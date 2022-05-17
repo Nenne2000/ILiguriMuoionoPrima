5c50eea6ee46d92d2dec67ce5da60371dae4a184
Frazzerz <michele.frattini00@gmail.com>
2022-05-17 16:14:20 +0200

    counter non funzionante

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
 
-function App() {
+
+const App = () => {
+
+  const [counter, setCounter] = useState(0);
+
+  const handlerCOunter = () => {
+    setCounter(counter++);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{counter}</p>
+      <button onClick={() => setCounter(counter++)}>Click me!</button>
     </div>
   );
 }
