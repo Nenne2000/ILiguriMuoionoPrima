b6dd0ff43efc2ee67d0e3f7a0793fb10798e466f
NicoInca <nicoinca00@hotmail.it>
2022-05-23 12:51:35 +0200

    countdwon

--- src/components/CountdownTimer.js
+++ src/components/CountdownTimer.js
@@ -2,31 +2,19 @@ import React from 'react';
 import DateTimeDisplay from './DateTimeDisplay';
 import { useCountdown } from './useCountdown';
 
-const ExpiredNotice = () => {
-  return (
-    <div className="expired-notice">
-      <span>Expired!!!</span>
-      <p>Please select a future date and time.</p>
-    </div>
-  );
-};
-
 const ShowCounter = ({ days, hours, minutes, seconds }) => {
   return (
     <div className="show-counter">
-      <a
+      <h1
         target="_blank"
         rel="noopener noreferrer"
         className="countdown-link"
       >
-        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />
-        <p>:</p>
-        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />
-        <p>:</p>
-        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />
-        <p>:</p>
+        <DateTimeDisplay value={days} type={'Days'} isDanger={days <= 3} />:
+        <DateTimeDisplay value={hours} type={'Hours'} isDanger={false} />:
+        <DateTimeDisplay value={minutes} type={'Mins'} isDanger={false} />:
         <DateTimeDisplay value={seconds} type={'Seconds'} isDanger={false} />
-      </a>
+        </h1>
     </div>
   );
 };
