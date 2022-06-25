41870d2d3cbda67cf0005fde44d2771b55679daa
NicoInca <nicoinca00@hotmail.it>
2022-06-25 11:38:29 +0200

    share buttons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,8 +10,31 @@ import MenuIcon from '@mui/icons-material/Menu';
 import ShareIcon from '@mui/icons-material/Share';
 import DrawerComp from "./Drawer";
 
+import {
+  FacebookShareButton, FacebookIcon,
+  TelegramShareButton, TelegramIcon,
+  TwitterShareButton, TwitterIcon,
+  WhatsappShareButton, WhatsappIcon
+} from "react-share";
+
+import Popper from '@mui/material/Popper';
+import Fade from '@mui/material/Fade';
+
 
 export default function ButtonAppBar() {
+
+  const [open, setOpen] = React.useState(false);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+    setOpen((previousOpen) => !previousOpen);
+  };
+
+  const canBeOpen = open && Boolean(anchorEl);
+  const id = canBeOpen ? 'transition-popper' : undefined;
+
+
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static"  sx={{ background: "#212121" }}>
@@ -27,9 +50,23 @@ export default function ButtonAppBar() {
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           </Typography>
-          <IconButton size="large" aria-label="search" color="inherit">
+          <IconButton size="large" aria-label="search" color="inherit" onClick={handleClick}>
             <ShareIcon />
           </IconButton>
+
+          <Popper id={id} open={open} anchorEl={anchorEl} transition >
+            {({ TransitionProps }) => (
+            <Fade {...TransitionProps} timeout={350}>
+              <Box sx={{ border: 1, p: 1, bgcolor: '#000000' }}>
+                <FacebookShareButton url="http://localhost:3000/"><FacebookIcon size={32} round={true} /></FacebookShareButton>
+                <TelegramShareButton url="http://localhost:3000/"><TelegramIcon size={32} round={true} /></TelegramShareButton>
+                <TwitterShareButton url="http://localhost:3000/"><TwitterIcon size={32} round={true} /></TwitterShareButton>
+                <WhatsappShareButton url="http://localhost:3000/"><WhatsappIcon size={32} round={true} /></WhatsappShareButton>
+              </Box>
+            </Fade>
+            )}
+          </Popper>
+
         </Toolbar>
       </AppBar>
     </Box>
